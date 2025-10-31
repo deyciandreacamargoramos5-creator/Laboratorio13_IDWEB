4e8a11c3ad778e8e64b9e62c82ef6eb4a8e28b90
deyciandreacamargoramos5-creator <dcamargor@unsa.edu.pe>
2025-10-31 17:21:47 -0500

    Subiendo onceavo commit: Ejercicio 11

---
+++ Ejercicio11.js
@@ -0,0 +1,17 @@
+function combinarCatalogos(tiendaA, tiendaB) {
+    const combinado = { ...tiendaA };
+    for (let producto in tiendaB) {
+        if (combinado.hasOwnProperty(producto)) {
+            combinado[producto] = Math.min(combinado[producto], tiendaB[producto]);
+        } else {
+            combinado[producto] = tiendaB[producto];
+        }
+    }
+    for (let producto in combinado) {
+        combinado[producto] = combinado[producto].toFixed(2);
+    }
+    return combinado;
+}
+const TiendaA = { laptop: 3500.5, mouse: 100.35, teclado: 250.9 };
+const TiendaB = { mouse: 95.2, monitor: 720.457, teclado: 260.1 };
+console.log(combinarCatalogos(TiendaA, TiendaB));
